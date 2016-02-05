2f013f86c30d5b9cce08c1d046f20734f1d92b4c
Silverwood <aaronpua@hotmail.com>
2016-02-05 11:50:01 -0800

    Merge origin/development into development
    Conflicts:
    	ChangeLog
    	application/config/routes.php

---
+++ application/controllers/Bingo.php
@@ -0,0 +1,15 @@
+<?php
+
+class Bingo extends Application {
+    
+    function __construct() {
+        parent::__construct();
+    }
+    
+    function index() {
+        $this->data['pagebody'] = 'justone';
+        $record = $this->quotes->get('5');
+        $this->data = array_merge($this->data, $record);
+        $this->render();
+    }
+}
