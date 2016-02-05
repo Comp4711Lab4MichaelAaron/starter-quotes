ec50418994ccff810418ec5cfdcbdaaec7a764da
Silverwood <aaronpua@hotmail.com>
2016-02-05 08:38:00 -0800

    First Feature Completed.

---
+++ application/controllers/First.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of First
+ *
+ * @author Aaron
+ */
+class First extends Application {
+    
+    function __construct() {
+        parent::__construct();
+    }
+    
+    function index() {
+        $this->data['pagebody'] = 'justone';
+        $record = $this->quotes->first();
+        $this->data = array_merge($this->data, $record);
+        $this->render();
+        
+    }
+}
